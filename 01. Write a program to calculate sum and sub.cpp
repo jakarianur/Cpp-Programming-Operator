a0ebf6556f598547a6d300667bc5884a4657d8f2
Md Jakaria Nur <mdjakarianur.in@gmail.com>
2024-05-16 19:24:55 +0600

    Create 01. Write a program to calculate sum and sub.cpp

---
+++ 01. Write a program to calculate sum and sub.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main () {
+int a, b, sum, sub;
+cout << "Enter your first number: ";
+cin >> a;
+cout << "Enter your second number: ";
+cin >> b;
+sum = a+b;
+sub = a-b;
+cout << "Summation = " << sum << endl;
+cout << "Subtraction = " << sub << endl;
+return 0; }
